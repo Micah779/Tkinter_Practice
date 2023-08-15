a2006609cf36eef4a9ee01633b92f89925ce6db1
Micah779 <contactmaldrich@gmail.com>
2023-08-14 23:51:43 -0400

    Update practice_functions.py

--- Mini_Projects/practice_functions.py
+++ Mini_Projects/practice_functions.py
@@ -4,14 +4,19 @@ from tkinter import ttk
 window = tk.Tk()
 window.title("buttons, functions and arguments")
 
+def button_func(entry_string):
+    print("Button was pressed")
+    print(entry_string.get())
+
 # widgets
+entry_string = tk.StringVar(value = 'Test')
 entry = ttk.Entry(window)
 entry.pack()
 
 button = ttk.Button(
     window, 
     text="Button",
-    command = button_func
+    command = button_func(entry_string)
     )
 button.pack()
 
