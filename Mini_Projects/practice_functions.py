bf5b99dc24b3f0c92014a2c70c2e99eb03237a82
Micah779 <contactmaldrich@gmail.com>
2023-08-14 23:50:08 -0400

    Update practice_functions.py

--- Mini_Projects/practice_functions.py
+++ Mini_Projects/practice_functions.py
@@ -4,7 +4,15 @@ from tkinter import ttk
 window = tk.Tk()
 window.title("buttons, functions and arguments")
 
+# widgets
+entry = ttk.Entry(window)
+entry.pack()
 
-
+button = ttk.Button(
+    window, 
+    text="Button",
+    command = button_func
+    )
+button.pack()
 
 window.mainloop()
