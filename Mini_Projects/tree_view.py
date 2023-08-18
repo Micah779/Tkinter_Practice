076330d5a335a70650e0b598da7efa6a336e33f0
Micah779 <contactmaldrich@gmail.com>
2023-08-17 23:47:34 -0400

    Create tree_view.py

---
+++ Mini_Projects/tree_view.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import ttk
+
+# window
+window = tk.Tk()
+window.title("Tree View")
+
+# data
+first_name = ['Bob','Jon','Adam','Micah','Henry','Lisa','Clark','Anna']
+last_name = ['Thai','Platt','Anderson','Colins','Martin','Chen','Zook','Aldrich']
+
+
+# run
+window.mainloop()
