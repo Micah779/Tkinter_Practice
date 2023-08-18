fa0bf6664257472c4ea6cd1004a0f87e4089daf9
Micah779 <contactmaldrich@gmail.com>
2023-08-17 23:52:07 -0400

    Update tree_view.py

--- Mini_Projects/tree_view.py
+++ Mini_Projects/tree_view.py
@@ -4,11 +4,22 @@ from tkinter import ttk
 # window
 window = tk.Tk()
 window.title("Tree View")
+window.geometry('600x400')
 
 # data
 first_name = ['Bob','Jon','Adam','Micah','Henry','Lisa','Clark','Anna']
 last_name = ['Thai','Platt','Anderson','Colins','Martin','Chen','Zook','Aldrich']
 
+# treeview
+table = ttk.Treeview(
+    window,
+    columns = ('first', 'last', 'email'),
+    show = 'headings'
+    )
+table.heading('first', text = 'First Name')
+table.heading('last', text = 'Surname')
+table.heading('email', text = 'Email')
+table.pack()
 
 # run
 window.mainloop()
