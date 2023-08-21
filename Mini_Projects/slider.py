0edd3ad1fd682f3424a1ccb96674f3ac7c0d7ffc
Micah779 <contactmaldrich@gmail.com>
2023-08-20 23:35:27 -0400

    Create slider.py

---
+++ Mini_Projects/slider.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import ttk
+
+# window
+window = tk.Tk()
+window.title('Sliders')
+window.geometry('400x600')
+# window
+
+                          
+
+# run
+window.mainloop()
+
